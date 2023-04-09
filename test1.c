23ef6c5f01400a8de86a4a202aaa90c8e7daa22b
wuwuwu <XW87878787@163.com>
2023-04-09 23:26:33 +0800

    密码

--- test1.c
+++ test1.c
@@ -17,23 +17,53 @@
 //
 //}
 
+//#include<stdio.h>
+//#include<string.h>
+//#include<windows.h>
+//int main()
+//{
+//	char arr1[] = "welcome to bit!!!!!!!";
+//	char arr2[] = "#####################";
+//	int left = 0;
+//	int right = strlen(arr1) - 1;
+//
+//	while(left <= right)
+//	{
+//		arr2[left] = arr1[left];
+//		arr2[right] = arr1[right];
+//      printf("%s\n",arr2);
+//      Sleep(1000); //睡眠1s
+//      system("cls");//清空屏幕
+//		printf("%s\n", arr2);
+//		left++;
+//		right--;
+//	}
+//	return 0;
+//}
+
 #include<stdio.h>
 #include<string.h>
 
 int main()
 {
-	char arr1[] = "welcome to bit!!!!!!!";
-	char arr2[] = "#####################";
-	int left = 0;
-	int right = strlen(arr1) - 1;
-
-	while(left <= right)
+	int i = 0;
+	char password[20] = { 0 };
+	scanf("%s", password);
+	for (i = 0; i <= 3; i++)
 	{
-		arr2[left] = arr1[left];
-		arr2[right] = arr1[right];
-		printf("%s\n", arr2);
-		left++;
-		right--;
+		if (strcmp(password, "123456") == 0)
+		{
+			printf("登陆成功\n");
+			break;
+		}
+		else
+		{
+			printf("输入错误，请重新输入！\n");
+		}
 	}
+	if (i == 3)
+		printf("三次密码均错误，退出程序\n");
 	return 0;
 }
+	
+	
