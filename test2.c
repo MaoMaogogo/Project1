155d6612c1c41c588d9f049abd4c9fdbab3b77a5
wuwuwu <XW87878787@163.com>
2023-04-10 23:07:19 +0800

    day2 函数

--- test2.c
+++ test2.c
@@ -101,3 +101,25 @@
 //	}
 //	return 0;
 //}
+
+//void Add(int* p)
+//{
+//	(*p)++;
+//}
+//
+//
+//int main()
+//{
+//	int num = 0;
+//
+//	Add(&num);
+//	printf("%d\n", num);//1
+//
+//	Add(&num);
+//	printf("%d\n", num);//2
+//
+//	Add(&num);
+//	printf("%d\n", num);//3
+//
+//	return 0;
+//}
