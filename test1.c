c3dc1bc7c01bf5d7e05d51719857ee81b8d69012
wuwuwu <XW87878787@163.com>
2023-04-10 16:07:24 +0800

    函数练习

--- test1.c
+++ test1.c
@@ -41,29 +41,29 @@
 //	return 0;
 //}
 
-#include<stdio.h>
-#include<string.h>
-
-int main()
-{
-	int i = 0;
-	char password[20] = { 0 };
-	scanf("%s", password);
-	for (i = 0; i <= 3; i++)
-	{
-		if (strcmp(password, "123456") == 0)
-		{
-			printf("登陆成功\n");
-			break;
-		}
-		else
-		{
-			printf("输入错误，请重新输入！\n");
-		}
-	}
-	if (i == 3)
-		printf("三次密码均错误，退出程序\n");
-	return 0;
-}
-	
-	
+//#include<stdio.h>
+//#include<string.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	char password[20] = { 0 };
+//	scanf("%s", password);
+//	for (i = 0; i <= 3; i++)
+//	{
+//		if (strcmp(password, "123456") == 0)
+//		{
+//			printf("登陆成功\n");
+//			break;
+//		}
+//		else
+//		{
+//			printf("输入错误，请重新输入！\n");
+//		}
+//	}
+//	if (i == 3)
+//		printf("三次密码均错误，退出程序\n");
+//	return 0;
+//}
+//	
+//	
