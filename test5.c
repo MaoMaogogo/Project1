6f7a70ddf3222d02e7a6dc86b132e32e9a31fdbb
wuwuwu <XW87878787@163.com>
2023-04-18 22:23:57 +0800

    字符

---
+++ test5.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	short s = 5;
+//	sizeof(s = a + 3);
+//	a + b;//值属性，类型属性int
+//	return 0;
+//
+//}
+
+//操作符优先级
+//int main()
+//{
+//	int a = 4;
+//	int b = 5;
+//	//int c=a+b*7//优先级决定顺序
+//	int c = a + b + 7;//
+//}
+
