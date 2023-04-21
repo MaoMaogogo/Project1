37d46bf4cabcbb851893c860bdcf60bf6a139ef5
wuwuwu <XW87878787@163.com>
2023-04-21 22:35:32 +0800

    指针

--- test6.c
+++ test6.c
@@ -13,10 +13,45 @@
 //	*vp=0；
 //}
 
-int main()
-{
-	int arr[10] = { 0 };
-	printf("%p\n", arr);//数组名是数组首元素的地址
-	printf("%p\n", &arr[0]);
-}
+//int main()
+
+//	int arr[10] = { 0 };
+//	printf("%p\n", arr);//数组名是数组首元素的地址
+//	printf("%p\n", &arr[0]);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int* p = arr;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		//printf("%p==%p\n", &arr[i], p + i);//首元素可以用*（p+i）表示
+//		*(p + i) = i;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d\n", *(p + i));
+//	}
+//	return 0;
+//}
+
+//交换
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int* p = arr;//数组名
+//	//[]是一个操作符2和arr是俩个操作数
+//	// a+b
+//	// b+a
+//	// 
+//	// 系统运算时arr[2]--->*(arr+2)
+//	// 
+//	//arr[2] =*(arr+2)= *(p + 2)==*(2+p)==*(2+arr)=2[arr]
+//	//2[arr]==*(2+arr)
+//	return 0;
+//}
+
 
